d9e849ea5dd316ad5693402b223c0392ac087ce2
Sbsbsv <sbsbsv002@gmail.com>
2023-06-27 13:02:40 +0530

    database

--- sclone/project/settings.py
+++ sclone/project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/db.sqlite3',
     }
 }
 
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT=os.path.join(BASE_DIR,'database/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
