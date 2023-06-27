3588e1c2538af331fa18b25ef0f8da3324e9fc54
Sbsbsv <sbsbsv002@gmail.com>
2023-06-28 02:43:27 +0530

    deploysh

--- sclone/deployment/deploy_prod.sh
+++ sclone/deployment/deploy_prod.sh
@@ -3,12 +3,11 @@ current_dir=$(pwd)
 echo $current_dir
 echo $USER
 
-ssh -T ubuntu@34.239.105.141 <<EOF
+
   cd project/Socialbook
   git pull
   cd sclone
   sudo docker-compose build
   sudo docker-compose up -d
-  
+
   exit
-EOF
