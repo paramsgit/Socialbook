4c300de4b2ead5382667e246712a915030de5b35
Sbsbsv <sbsbsv002@gmail.com>
2023-06-28 10:29:34 +0530

    rem

--- sclone/deployment/deploy_prod.sh
+++ sclone/deployment/deploy_prod.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-current_dir=$(whoami)
+current_dir=$(pwd)
 echo $current_dir
 echo $USER
 
 ssh -T ubuntu@34.239.105.141 <<EOF
   cd project/Socialbook
   git pull
-  source sclone/env/bin/activate
-  pip install -r requirements.txt
-  python manage.py makemigrations
-  python manage.py migrate  --run-syncdb
-  deactivate
+  cd sclone
   sudo docker-compose build
   sudo docker-compose up -d
   sudo service nginx restart
